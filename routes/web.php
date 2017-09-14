ffa7526a1f15b4efe70f34f1dd586047ad4d338f
agus24 <agusx244@gmail.com>
2017-09-14 22:46:14 +0700

    hotfix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With, X-XSRF-TOKEN, Authorization');
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +17,30 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/password/{password}' , function($password) {
+    return bcrypt($password);
+});
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(["prefix" => 'api'], function() {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('login','api\UserController@login');
+    Route::get('getKitab','api\AlkitabController@getKitab');
+    Route::get('getPasalAyat/{kitab}','api\AlkitabController@getPasalAyat');
+    Route::get('getFirman/{kitab}/{pasal}/{ayat}','api\AlkitabController@getFirman');
 });
